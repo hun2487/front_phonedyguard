d5891fc8e97ada0ac8fd005db9e4a56f34b46e0c
hun2487 <hun2487@naver.com>
2022-04-26 18:25:19 +0900

    ✨ feat: 첫 화면 구현, 버튼 연결
    첫 화면 구현, 로그인 버튼 연결

--- app/src/main/java/com/example/phonedyguard/MainActivity.java
+++ app/src/main/java/com/example/phonedyguard/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.phonedyguard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.first);
+
+        Button signbt = (Button) findViewById(R.id.loginButton);
+        signbt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
